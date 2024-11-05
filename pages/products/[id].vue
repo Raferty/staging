fbdd76cfec90a54feb033a5b5a95638d3598ce2f
София <sofiapasko@yandex.ru>
2024-11-05 17:20:54 +0300

    some commet here

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,13 +1,16 @@
-<template>Странца продукта</template>
-
-<script setup>
-const route = useRoute();
-
-console.log("Текущий ID продукта", route.params.id);
-
-// Эндпоинт базы данных https://fakestoreapi.com/products/1
-// Сделать запрос к базе данных с помощью https://nuxt.com/docs/getting-started/data-fetching
-// Вывести карточку продукта в шаблон с помощью компонента ProductCard
-</script>
-
-<style lang="scss" scoped></style>
+<template>Странца продукта
+    <ProductCard :id="product.id" :title="product.title" :category="product.category" :price="product.price"
+      :image="product.image" />
+  </template>
+  
+  <script setup>
+  const route = useRoute();
+  const products_id = route.params.id
+  console.log("Текущий ID продукта", products_id);
+  const { data: product } = await useFetch('https://fakestoreapi.com/products/${products_id}')
+  // Эндпоинт базы данных https://fakestoreapi.com/products/1
+  // Сделать запрос к базе данных с помощью https://nuxt.com/docs/getting-started/data-fetching
+  // Вывести карточку продукта в шаблон с помощью компонента ProductCard
+  </script>
+  
+  <style lang="scss" scoped></style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,10 +1,35 @@
 <template>
-    <UiTitle tag="h1">Filter</UiTitle>
-    <ProductsFilter />
-    <UiTitle tag="h1">Products</UiTitle>
-    <ProductsList />
-  </template>
-  
-  <script setup></script>
-  
-  <style lang="scss" scoped></style>
+  <UiTitle tag="h1">Filter</UiTitle>
+  {{ filters }}
+  <ProductsFilter @update-filter="filterHandle" />
+  <UiTitle tag="h1">Products</UiTitle>
+  <ProductsList :products="filterProducts" />
+</template>
+
+<script setup>
+// Связка двух компонентов
+
+const { data: ProductsData } = await useFetch(
+  `https://fakestoreapi.com/products`
+);
+
+const filters = ref();
+
+const filterHandle = (event) => {
+  filters.value = event;
+};
+
+const filterProducts = computed(() => {
+  return filters.value
+    ? ProductsData.value
+        .filter((elem) => elem.category === filters.value?.category)
+        .filter(
+          (product) =>
+            product.price > filters.value?.price.from &&
+            product.price < filters.value?.price.to
+        )
+    : ProductsData.value;
+});
+</script>
+
+<style lang="scss" scoped></style>
